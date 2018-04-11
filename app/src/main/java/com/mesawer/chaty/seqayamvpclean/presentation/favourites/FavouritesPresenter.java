bbdf25ef0ad88ca534a8329c45966d27c2af4d34
AhmedSalahEldinIbrahim <34102061+AhmedSalahEldinIbrahim@users.noreply.github.com>
2018-04-11 14:29:58 +0200

    change salah

--- app/src/main/java/com/mesawer/chaty/seqayamvpclean/presentation/favourites/FavouritesPresenter.java
+++ app/src/main/java/com/mesawer/chaty/seqayamvpclean/presentation/favourites/FavouritesPresenter.java
@@ -1,15 +1,8 @@
 package com.mesawer.chaty.seqayamvpclean.presentation.favourites;
 
-import com.mesawer.chaty.seqayamvpclean.domain.entity.Fav;
-
 public class FavouritesPresenter implements FavouritesContract.Presenter{
     @Override
     public void getFavourites() {
-
-    }
-
-    @Override
-    public void addFavourite(Fav fav) {
 
     }
 
