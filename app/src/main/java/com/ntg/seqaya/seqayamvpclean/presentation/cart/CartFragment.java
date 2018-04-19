60a8e126104a2fafb4ae20b94a7b1998f33a1951
AhmedSalahEldinIbrahim <34102061+AhmedSalahEldinIbrahim@users.noreply.github.com>
2018-04-19 15:36:15 +0200

    cash on delivery

--- app/src/main/java/com/ntg/seqaya/seqayamvpclean/presentation/cart/CartFragment.java
+++ app/src/main/java/com/ntg/seqaya/seqayamvpclean/presentation/cart/CartFragment.java
@@ -2,7 +2,6 @@ package com.ntg.seqaya.seqayamvpclean.presentation.cart;
 
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,6 +20,7 @@ import com.ntg.seqaya.seqayamvpclean.base.BaseFragment;
 import com.ntg.seqaya.seqayamvpclean.domain.entity.CartItem;
 import com.ntg.seqaya.seqayamvpclean.domain.entity.Order;
 import com.ntg.seqaya.seqayamvpclean.domain.entity.User;
+import com.ntg.seqaya.seqayamvpclean.presentation.DeliveryTimeFragment;
 import com.ntg.seqaya.seqayamvpclean.presentation.location.OrderMapActivity;
 import com.ntg.seqaya.seqayamvpclean.presentation.main.CartItemsCountListener;
 import com.ntg.seqaya.seqayamvpclean.presentation.main.MainActivity;
@@ -114,12 +114,12 @@ public class CartFragment extends BaseFragment implements CartAdapter.TotalListe
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == Activity.RESULT_OK) {
-//            DeliveryTimeFragment deliveryTimeFragment = new DeliveryTimeFragment();
-//            Bundle args = new Bundle();
-//            args.putSerializable(MainActivity.ORDER, data.getSerializableExtra(MainActivity.ORDER));
-//            deliveryTimeFragment.setArguments(args);
-//            getActivity().getFragmentManager().beginTransaction().addToBackStack(null)
-//                    .replace(R.id.container, deliveryTimeFragment).commitAllowingStateLoss();
+            DeliveryTimeFragment deliveryTimeFragment = new DeliveryTimeFragment();
+            Bundle args = new Bundle();
+            args.putSerializable(MainActivity.ORDER, data.getSerializableExtra(MainActivity.ORDER));
+            deliveryTimeFragment.setArguments(args);
+            getActivity().getFragmentManager().beginTransaction().addToBackStack(null)
+                    .replace(R.id.container, deliveryTimeFragment).commitAllowingStateLoss();
         }
     }
 
--- app/src/main/java/com/ntg/seqaya/seqayamvpclean/presentation/paymentmethod/CashOnDeliveryFragment.java
+++ app/src/main/java/com/ntg/seqaya/seqayamvpclean/presentation/paymentmethod/CashOnDeliveryFragment.java
@@ -1,21 +1,20 @@
 package com.ntg.seqaya.seqayamvpclean.presentation.paymentmethod;
 
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.ntg.seqaya.seqayamvpclean.R;
 import com.ntg.seqaya.seqayamvpclean.base.BaseFragment;
 import com.ntg.seqaya.seqayamvpclean.data.datasource.remote.network.ApiClient;
 import com.ntg.seqaya.seqayamvpclean.data.datasource.remote.network.ProductService;
 import com.ntg.seqaya.seqayamvpclean.domain.entity.Order;
-import com.ntg.seqaya.seqayamvpclean.domain.entity.PaymentMethod;
 import com.ntg.seqaya.seqayamvpclean.presentation.main.MainActivity;
 import com.ntg.seqaya.seqayamvpclean.presentation.ordersummery.SummaryOrderFragment;
 import com.ntg.seqaya.seqayamvpclean.utils.ViewUtil;
@@ -27,66 +26,44 @@ import retrofit2.Response;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PaymentFragment extends BaseFragment implements View.OnClickListener {
-
-    RelativeLayout bankTransfer, sadad, creditCard, cashOnDelivery;
+public class CashOnDeliveryFragment extends BaseFragment implements View.OnClickListener {
+    View view;
     Order order;
+    TextView deliveryLocation, phone_number;
+    TextView deliveryLocationText, customer_number;
+    Button confim;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_payment, container, false);
-
-        ViewUtil.setupActionBar(getActivity(), getString(R.string.payment_method));
-        bankTransfer = view.findViewById(R.id.bankTransfer);
-        sadad = view.findViewById(R.id.sadad);
-        creditCard = view.findViewById(R.id.creditCard);
-        cashOnDelivery=view.findViewById(R.id.cashOnDelivery);
-        ViewUtil.addShadowToView(getActivity(), bankTransfer);
-        ViewUtil.addShadowToView(getActivity(), sadad);
-        ViewUtil.addShadowToView(getActivity(), creditCard);
-        ViewUtil.addShadowToView(getActivity(), cashOnDelivery);
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_cash_on_delivery, container, false);
+        ViewUtil.setupActionBar(getActivity(), getString(R.string.cash_on_delivery));
+        deliveryLocation = view.findViewById(R.id.deliveryLocation);
+        phone_number = view.findViewById(R.id.phone_number);
+        confim = view.findViewById(R.id.confim);
+        deliveryLocationText = view.findViewById(R.id.deliveryLocationText);
+        customer_number = view.findViewById(R.id.customer_number);
 
         order = (Order) getArguments().getSerializable(MainActivity.ORDER);
-
+        deliveryLocation.setText(order.getLocation().getAddress());
         return view;
+
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        bankTransfer.setOnClickListener(this);
-        sadad.setOnClickListener(this);
-        creditCard.setOnClickListener(this);
-        cashOnDelivery.setOnClickListener(this);
+        confim.setOnClickListener(this);
     }
 
-    @SuppressLint("WrongConstant")
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.bankTransfer:
-                order.setPaymentMethod(PaymentMethod.BANK_TRANSFER);
+            case R.id.confim:
                 navigateToOrderSummery(order);
                 break;
-            case R.id.sadad:
-                order.setPaymentMethod(PaymentMethod.SADAD);
-                navigateToOrderSummery(order);
-                break;
-            case R.id.creditCard:
-                order.setPaymentMethod(PaymentMethod.CREDIT_CARD);
-                navigateToOrderSummery(order);
-                break;
-            case R.id.cashOnDelivery:
-                order.setPaymentMethod(PaymentMethod.CASH_ON_DELIVERY);
-                CashOnDeliveryFragment cashOnDeliveryFragment = new CashOnDeliveryFragment();
-                Bundle args = new Bundle();
-                args.putSerializable(MainActivity.ORDER, order);
-                cashOnDeliveryFragment.setArguments(args);
-                getActivity().getFragmentManager().beginTransaction()
-                        .addToBackStack(null).replace(R.id.container, cashOnDeliveryFragment).commit();
-
         }
     }
 
